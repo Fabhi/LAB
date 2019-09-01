c1ac2cefa5e3d3769defe8edc1a170a321d4b23b
Fabhi <43674998+Fabhi@users.noreply.github.com>
2019-09-01 14:43:48 +0530

    nTranspose Cypher

---
+++ DS/Lab4/3.cpp
@@ -0,0 +1,77 @@
+//Program to find Transpose of a matrix using Sparse Row Method
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+class Sparse{
+    public:
+    int row;
+    int column;
+    int value;
+    void set(int r, int c, int v){
+        row=r;column=c;value=v;
+    }
+    void nTranspose(Sparse a[], Sparse b[]){
+        int r=a[0].row;
+        int c=a[0].column;
+        int v=a[0].value;
+        b[0].row=c;
+        b[0].column=r;
+        b[0].value=v;
+        int k=1;
+        for(int i=0;i<c;i++){
+            for(int j=1;j<=v;j++){
+                if(a[j].column==i){
+                    b[k].row=a[j].column;
+                    b[k].column=a[j].row;
+                    b[k++].value=a[j].value;
+                }
+            }
+        }
+
+    }
+    void display(){
+        cout<<row<<' '<<column<<' '<<value<<" "<<endl;
+    }
+
+};
+
+
+int main(){
+    int m,n;
+    cout<<"Enter the size of the matrix:"<<endl;
+    cin>>m>>n;
+    int arr[m][n];
+    int count=0;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cout<<"Enter ["<<i<<"]["<<j<<"]:";
+            cin>>arr[i][j];
+            if(arr[i][j]!=0)
+                count++;
+        }
+    }
+    //Developing a sparse matrix
+    Sparse s[count+1];
+    s[0].set(m,n,count);
+    int k=0;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(arr[i][j]!=0){
+                s[++k].set(i,j,arr[i][j]);
+            }
+        }
+    }
+    cout<<"Matrix before Transposing"<<endl;
+    for(int k=0;k<=count;k++){
+        s[k].display();
+    }
+
+    Sparse b[count+1], temp;
+    temp.nTranspose(s,b);
+    cout<<"Matrix after Transposing"<<endl;
+    for(int k=0;k<=count;k++){
+        b[k].display();
+    }
+}
