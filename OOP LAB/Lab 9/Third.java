f0e148745b79809b63bffb74667c13bbb104c297
Fabhi <43674998+Fabhi@users.noreply.github.com>
2019-10-02 21:20:40 +0530

    Java Lab9 Commit

--- OOP LAB/Lab 9/Third.java
+++ OOP LAB/Lab 9/Third.java
@@ -1,30 +1,44 @@
 import java.util.Scanner;
+import java.util.Arrays;
 public class Third{
-    static void sorting(StringBuffer s){
-        char c;
-        String s1=s.toString();
-        for(int i=0;i<s1.length();i++){
-            for(int j=i+1;j<s1.length();i++){
-                if(s.charAt(i)>s1.charAt(j)){
-                    c=s.charAt(i);
-                    s1.replace(s.charAt(i),s.charAt(j));
-                    s1.replace(s.charAt(j),c);
-                }
-            }
-        }
-        System.out.println(s1);
-    }
-    public static void main(String aargs[]){
-        System.out.print("Enter string:");
+    public static void main(String args[]){
         Scanner scan= new Scanner(System.in);
-        StringBuffer sb= new StringBuffer();
-        StringBuffer s=new StringBuffer(scan.nextLine());
-        sb.append(s);
-        sb.reverse();
-        if(sb.toString().equals(s.toString()))
-            System.out.println("String is a palindrome");
-        else
-            System.out.println("Not a palindrome");
-        sorting(s);
+        System.out.print("Enter the string:");
+        String strb= scan.nextLine();
+        String strb2= ((new StringBuffer(strb)).reverse()).toString();
+        System.out.println("1.Check for Palindrome");
+        System.out.println("2.Arrange in alphabetical order");
+        System.out.println("3.Reverse the string");
+        System.out.println("4.Concatenate original and reversed");
+        System.out.println("5.Exit");
+        int choice;
+        while(true){
+            System.out.print(">");
+            choice=scan.nextInt();
+            switch (choice){
+                case 1:
+                    if(strb.equals(strb2))
+                        System.out.println("String is a palindrome");
+                    else
+                        System.out.println("Not a palindrome");
+                    break;
+                case 2:
+                    char[] c= strb.toCharArray();
+                    Arrays.sort(c);
+                    System.out.println(new String(c));
+                    break;
+                case 3:
+                    System.out.println(strb2);
+                    break;
+                case 4:
+                    System.out.println(strb.concat(strb2));
+                    break;
+                case 5:
+                    scan.close();
+                    return;
+                default:
+                    System.out.println("Invalid Choice");
+            }   
+        }
     }
 }
