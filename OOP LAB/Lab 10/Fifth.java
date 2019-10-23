25ea4ddc459134c091782fedba30a62a4636953c
Abhishek Srivastva <fabhi@protonmail.com>
2019-10-23 09:57:31 +0530

    Tenth

--- OOP LAB/Lab 10/Fifth.java
+++ OOP LAB/Lab 10/Fifth.java
@@ -3,13 +3,26 @@ class Student implements Serializable{
     private static final long serialVersionUID = 1L;
     double percentage;
     char grade;
-    Student(){
-        
+    int roll;
+    Student(double p, int r,char g){
+        percentage=p;
+        grade=g;
+        roll=r;
+    }
+    void display(){
+        System.out.println(roll+" "+grade+" "+percentage);
     }
 }
 
 class Fifth{
-    public static void main(String args[]){
-
+    public static void main(String args[])throws Exception{
+        ObjectOutputStream oos= new ObjectOutputStream(new FileOutputStream(new File("Fifth.txt")));
+        Student s= new Student(Double.parseDouble(args[0]), Integer.parseInt(args[1]), args[2].charAt(0));
+        oos.writeObject(s);
+        oos.close();
+        ObjectInputStream ois= new ObjectInputStream(new FileInputStream(new File("Fifth.txt")));
+        Student s1= (Student) ois.readObject();
+        s1.display();
+        ois.close();
     }
 }
--- OOP LAB/Lab 10/First.java
+++ OOP LAB/Lab 10/First.java
@@ -1,9 +1,5 @@
 import java.util.Scanner;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.EOFException;
+import java.io.*;
 public class First{
     public static void main(String args[]) throws IOException, EOFException{
         Scanner scan= new Scanner(System.in);
@@ -52,8 +48,8 @@ public class First{
             case 4:
                 return;
             default:
-                System.out.println("Invalid Choice!");
-            }
+                System.out.println("Invalid Choice!");          
+            }  
         }
     }
 }
