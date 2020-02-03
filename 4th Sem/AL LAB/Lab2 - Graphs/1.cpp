4acba909a60ecdd5816b6adedd21fd1512356161
FabhiUXPS <fabhi@protonmail.com>
2020-02-03 11:45:15 +0530

    Incomplete 2.cpp

--- 4th Sem/AL LAB/Lab2 - Graphs/1.cpp
+++ 4th Sem/AL LAB/Lab2 - Graphs/1.cpp
@@ -1,4 +1,4 @@
-// Program to perform traversals(BFS and DFS)
+// Program to perform traversals(BFS and DFS) using 1-indexing
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
@@ -9,11 +9,11 @@ class UGraph{
     public:
     UGraph(int numb){
         n = numb;
-        adj = new int*[n];
-        for(int i=0;i<n; ++i)
-            adj[i] = new int[n];
-        for(int i=0;i<n;++i){
-            for(int j=0;j<n;j++)
+        adj = new int*[n+1];
+        for(int i=0;i<n+1; ++i)
+            adj[i] = new int[n+1];
+        for(int i=0;i<n+1;++i){
+            for(int j=0;j<n+1;j++)
                 adj[i][j] = 0;
         }
     }
@@ -22,39 +22,39 @@ class UGraph{
         adj[dest][src]=1;
     }
     void DFS(int source){
-        bool visited[n] = {false};
+        bool visited[n+1] = {false};
         stack <int> s;
         s.push(source);
         visited[source] = true;
         while(!s.empty()){
             int curr = s.top();
             s.pop();
-            for(int i=0;i<n;i++){
+            for(int i=1;i<n+1;i++){
                 if(adj[curr][i]==1 && !visited[i]){
                     visited[i]=true;
                     s.push(i);
-                    cout<<i<<" ";
                 }
             }
+            cout<<curr<<" ";
         }
         cout<<endl;
     }
 
     void BFS(int source){
-        bool visited[n]={false};
+        bool visited[n+1]={false};
         queue <int> s;
         s.push(source);
         visited[source]=true;
         while(!s.empty()){
             int c=s.front();
             s.pop();
-            for(int i=0;i<n;i++){
+            for(int i=1;i<n+1;i++){
                 if(adj[c][i]==1 && !visited[i]){
                     visited[i]=true;
                     s.push(i);
-                    cout<<i<<" ";
                 }
             }
+            cout<<c<<" ";
         }
         cout<<endl;
     }
@@ -62,14 +62,15 @@ class UGraph{
 
 int main(){
     UGraph G(6);
-    G.addVertex(1,0);
-    G.addVertex(1,2);
-    G.addVertex(0,3);
-    G.addVertex(3,4);
-    G.addVertex(2,0);
+    G.addVertex(6,4);
+    G.addVertex(4,3);
+    G.addVertex(3,2);
+    G.addVertex(2,5);
+    G.addVertex(2,1);
     G.addVertex(1,5);
+    G.addVertex(4,5);
     cout<<"BFS: ";
-    G.BFS(2);
+    G.BFS(1);
     cout<<"DFS: ";
-    G.DFS(2);
+    G.DFS(1);
 }
