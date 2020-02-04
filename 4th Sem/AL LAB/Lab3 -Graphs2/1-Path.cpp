7aa54437315b5c09b1f5f080da2bb2014d50352e
Fabhi <43674998+Fabhi@users.noreply.github.com>
2020-02-05 03:03:10 +0530

    Complete

---
+++ 4th Sem/AL LAB/Lab3 -Graphs2/1-Path.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+class Node{
+	public:
+	int val;
+	Node* next = NULL;
+};
+
+void add(Node*& head, int val){
+	if(!head){
+		head = new Node;
+		head->val = val;
+		return;
+	}
+	Node* temp = head;
+	while(temp->next != NULL)
+		temp = temp->next;
+		
+	Node* newn = new Node;
+	newn->val = val;
+	temp->next = newn;
+}
+
+bool path(Node* graph[], int visited[], int src, int dest){
+	Node* temp = graph[src];
+	while(temp != NULL){
+		if(visited[temp->val] == 0){
+			if (temp->val == dest || path(graph, visited, temp->val, dest)){
+				cout << temp->val << " ";
+				visited[temp->val] = 1;
+				return true;
+			}
+		}
+		temp = temp->next;
+	}
+	return false;
+}
+
+int main(){
+	int n;
+    cout<<"Enter size:";
+    cin>>n;
+	Node* graph[n]= {NULL};
+    cout<<"Enter edges:"<<endl;
+    int a=0,b=0;
+    while(true){
+        cin>>a>>b;
+        if(a!=-1 && b!=-1)
+            add(graph[a], b);
+        else
+            break;
+    }
+    cout<<"Path between:";
+    cin>>a>>b;
+    int visited[n]={0};
+    cout<<path(graph,visited,a,b);
+}
