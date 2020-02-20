9c2d568f36e73948802198ed827ab282cebddbc6
FabhiUXPS <fabhi@protonmail.com>
2020-02-20 05:51:21 +0530

    Too Late Commit

--- 4th Sem/OS LAB/Lab5/2.c
+++ 4th Sem/OS LAB/Lab5/2.c
@@ -1,40 +1,33 @@
 #include <stdio.h>
 #include <sys/types.h>  //Contains data type such as pid_t
-#include <sys/wait.h>  //Contains the wait system call
-#include <unistd.h>  //Contains the API for POSIX system calls
+#include <sys/wait.h>   //Contains the wait system call
+#include <unistd.h>     //Contains the API for POSIX system calls
 #include <string.h>
-int main(int argc, char ** argv){
+int main(int argc, char** argv){
     int i, j;
-    int n=argc-1;
-    char *temp;
-    char **array;
-    for(i=0;i<n;i++){
-        strcpy(array[i],argv[i+1]);
-    }
-    int f = fork();
-    if(f==0){
-        printf(" |->Inside Child \n");
-        
-        for(i=0;i<n;i++){
-            printf("%s",array[i]);
-        }
-        for(i=0;i<n;i++){
-            for(j=1;j<n-i-1;j++){
-                if(strcmp(array[j],array[j+1])){
-                    strcpy(temp,array[j]);
-                    strcpy(array[j],array[j+1]);
-                    strcpy(array[j+1],temp);
+    char temp[100];
+    if(fork()==0){
+        printf("Sorted Array: ");
+        for(i=1;i<argc;i++){
+            for(j=1;j<argc-i-1;j++){
+                if(strcmp(argv[j],argv[j+1])>0){
+                    strcpy(temp,argv[j]);
+                    strcpy(argv[j],argv[j+1]);
+                    strcpy(argv[j+1],temp);
                 }
             }
-            printf(" |->%s", array[i]);
+            printf("%s ", argv[i]);
         }
+        printf("\n");
         return 1;
     }
-    if(f>0){
-        printf("Inside Parent \n");
+    if(fork()>0){
         wait(NULL);
+        printf("Original Array: ");
         for(i=1;i<argc;i++){
-            printf("%s \n", argv[i]);
+            printf("%s ", argv[i]);
         }
+        printf("\n");
+        return 1;
     }
 }
