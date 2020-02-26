eb22a5c584988548d172f8bc3716675eeae1a26f
FabhiUXPS <fabhi@protonmail.com>
2020-02-26 23:28:02 +0530

    Fix

--- 4th Sem/OS LAB/Lab7/1.c
+++ 4th Sem/OS LAB/Lab7/1.c
@@ -2,51 +2,52 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <semaphore.h>
-int bufferBound;
+
+int capacity;
 int *queue;
-int queueLength;
 int max;
-sem_t semaphore,bound;
+sem_t full, empty;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void *producer( void * param){
+    int in = 0;
     for ( int i=0; i<max; i++ ){
-        // Add item to queue
-        sem_wait(&bound);
+        sem_wait(&empty);  //Wait for post on empty
         pthread_mutex_lock( &mutex );
-        queue[ queueLength++ ] = i;
+        queue[in] = i;
+        in = (in+1)%capacity;
         printf("Produced %d\n", i);
         pthread_mutex_unlock( &mutex );
-        // Signal semaphore
-        sem_post( &semaphore );
+        sem_post( &full );  //Signal full
     }
 }
 void *consumer(void * param){
+    int out = 0;
     for ( int i=0; i<max; i++ ){
-        int item;
-        // Wait if nothing in queue
-        sem_wait(&semaphore);
+        sem_wait(&full); //Wait for post on full
         pthread_mutex_lock( &mutex );
-        item = queue[ -- queueLength ];
-        printf("Consumed %i\n", item);
+        printf("Consumed %i\n", queue[out]);
+        out= (out+1)%capacity;
         pthread_mutex_unlock( &mutex );
-        sem_post(&bound);
+        sem_post(&empty);  //Signal full
     }
 }
 int main(){
     printf("Enter the size of the buffer: ");
-    scanf("%d", &bufferBound);
-    printf("Enter the maximum items allowed to be produced: ");
+    scanf("%d", &capacity);
+    printf("Enter the number of items to produce: ");
     scanf("%d", &max);
-    queue = malloc((bufferBound) *sizeof(int));
+    queue = malloc((capacity) *sizeof(int));
+
     pthread_t threads[2];
-    sem_init( &semaphore, 0, 0 );
-    sem_init( &bound,0,bufferBound);
+    sem_init( &full, 0, 0 );
+    sem_init( &empty, 0, capacity);
     pthread_create( &threads[0], 0, producer, 0 );
     pthread_create( &threads[1], 0, consumer, 0 );
     pthread_join( threads[0], 0 );
     pthread_join( threads[1], 0 );
-    sem_destroy( &semaphore );
+    sem_destroy( &full );
+    sem_destroy( &empty );
 }
 
 // Compile using -lpthread -lrt flag
